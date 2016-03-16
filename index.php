9dd2dc149fe1b0c10c12ea98f8dc4ba1efade372
Ricardo <ricardo.b.silva13@gmail.com>
2016-03-15 21:36:38 -0300

    estrutura musicas

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php include './style/template.php'; ?>
 
-<div class="col-sm-5 col-sm-offset-3" style="margin-top: 250px;">
+<div class="col-sm-5 col-sm-offset-3" style="margin-top: 230px;">
     <h2>Discográfica PHP</h2> - <h4>login</h4>
     <input name="email" class="form-control">
     <input name="senha" class="form-control">
---
+++ view/index_musicas.php
@@ -0,0 +1,18 @@
+<?php include '/../style/template.php';?>
+
+
+<div class="col-sm-10 col-sm-offset-1">
+    <h3>Musicas</h3>
+    <table class="table table-striped">
+        <tr>
+            <td>id</td>
+            <td>nome</td>
+            <td>autor_id</td>
+        </tr>
+        <tr>
+            <td>1</td>
+            <td>Musica1</td>
+            <td>10</td>
+        </tr>
+    </table>
+</div>
